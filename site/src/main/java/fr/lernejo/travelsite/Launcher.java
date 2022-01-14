58e6568fbdb6effdbb8695f16ccf170c3c816167
ahmedglas <ahmedscon@gmail.com>
2022-01-14 03:04:40 +0100

    Partie 3 - Client add Models

--- site/src/main/java/fr/lernejo/travelsite/Launcher.java
+++ site/src/main/java/fr/lernejo/travelsite/Launcher.java
@@ -2,9 +2,23 @@ package fr.lernejo.travelsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @SpringBootApplication
 public class Launcher {
+
+    @Bean
+    PredictionEngineClient predictionEngineClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://localhost:7080")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+        return retrofit.create(PredictionEngineClient.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Launcher.class, args);
